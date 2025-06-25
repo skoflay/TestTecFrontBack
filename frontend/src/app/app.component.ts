d62b0011722a517cf1dd86763b41cb2c9746e811
souhail <boullilsouhil@gmail.com>
2025-06-25 15:37:33 +0100

    component ts

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ProjectService } from './services/project.service';
+import { Project } from './models/project.model';
 
 @Component({
-  standalone: true,
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  imports: [RouterModule]
+  template: `
+    <h1>Liste des projets</h1>
+    <button (click)="loadProjects()">Charger projets</button>
+    <ul>
+      <li *ngFor="let p of projects">
+        {{p.name}} ({{p.description}})
+      </li>
+    </ul>
+  `
 })
-export class AppComponent {
-  title = 'gestion-projets';
+export class AppComponent implements OnInit {
+  projects: Project[] = [];
+
+  constructor(private projectService: ProjectService) {}
+
+  ngOnInit() {}
+
+  loadProjects() {
+    this.projectService.getAllProjects().subscribe(
+      data => this.projects = data,
+      err => console.error('Erreur:', err)
+    );
+  }
 }
